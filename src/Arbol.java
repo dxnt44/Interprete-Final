9830d140f35a450becb14ce7c6956b371b9dd36e
dxnt44 <trejod40@gmail.com>
2023-06-30 01:09:31 -0600

    Creación a partir de código recibido
    Inicio

---
+++ src/Arbol.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arbol {
+    private final Nodo raiz;
+
+    public Arbol(Nodo raiz){
+        this.raiz = raiz;
+    }
+
+    public void recorrer(){
+        for(Nodo n : raiz.getHijos()){
+            Token t = n.getValue();
+            switch (t.tipo){
+                // Operadores aritméticos
+                case SUMA:
+                case RESTA:
+                case MULTIPLICACION:
+                case DIVISION:
+                    SolverAritmetico solver = new SolverAritmetico(n);
+                    Object res = solver.resolver();
+                    System.out.println(res);
+                    break;
+
+                case VAR:
+                    // Crear una variable. Usar tabla de simbolos
+                    break;
+                case IF:
+                    break;
+
+            }
+        }
+    }
+
+}
+
---
+++ src/TipoToken.java
@@ -0,0 +1,14 @@
+public enum TipoToken {
+    IDENTIFICADOR, NUMERO, CADENA,
+
+    // Palabras reservadas
+    IF, VAR, PRINT, ELSE,
+
+    // Caracteres
+    SUMA, RESTA, MULTIPLICACION, DIVISION, IGUAL,
+    LPAREN, RPAREN, LBRACE, RBRACE, SEMICOLON,
+    MAYOR, MAYOR_IGUAL,
+
+    // Final de cadena
+    EOF
+}
